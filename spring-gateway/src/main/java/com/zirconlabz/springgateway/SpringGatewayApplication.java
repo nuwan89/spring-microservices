09b360c05fea40fef32cccf681e31a2d615208b1
Thilanka <catchthila@gmail.com>
2022-06-14 16:52:46 +0530

    Adds spring cloud gateway

---
+++ spring-gateway/src/main/java/com/zirconlabz/springgateway/SpringGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.zirconlabz.springgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringBootApplication
+public class SpringGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGatewayApplication.class, args);
+	}
+
+}
